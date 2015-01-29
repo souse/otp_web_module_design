cd27a8d97e56eb41bf68c389991a6f2ba40504ab
tianyingchun <tianyingchun@outlook.com>
2015-01-29 16:34:58 +0800

    updates

--- scripts/seajs/ui-plugins/otp/index.js
+++ scripts/seajs/ui-plugins/otp/index.js
@@ -1,3 +1,7 @@
+var $ = require("jquery");
+var OtpImageSuite = require("./OtpImageSuite");
+var otpService = require("./otpAPI");
+
 var otp;
 
-module.exports = otp;
+module.exports = OtpImageSuite;
